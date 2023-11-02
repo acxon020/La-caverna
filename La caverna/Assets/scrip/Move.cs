6e0d184f8d95a65fdfcae015f430225dc4970ffb
Miguelqo777 <146156517+Miguelqo777@users.noreply.github.com>
2023-11-01 21:25:37 -0700

    Update Move.cs
    codigo de salto y movimiento

--- La caverna/Assets/scrip/Move.cs
+++ La caverna/Assets/scrip/Move.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Move : MonoBehaviour
 {
-    public float velocidad;
+    //esto aparece en script
+    public float Speed;
+    public float JumpForce;
 
-    // Update is called once per frame
+    //esto lo mandamos a llamar adentro del juego
+    private Rigidbody2D rb2d; //gravedad
+    private float Horizontal;//movimiento horizontal
+    private bool Grounded; //piso
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); //para que al inicio tenga gravedad
+
+    }
+    //codigo de salto
     void Update()
     {
-        ProcesarMovimiento();
+        Horizontal = Input.GetAxisRaw("Horizontal"); //movimiento horizontal
+
+        Debug.DrawRay(transform.position, Vector3.down * 1.0f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+
+        }
+        else
+        {
+            Grounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && Grounded) // brinco
+        {
+            rb2d.AddForce(Vector2.up * JumpForce);
+        }
     }
 
-    void ProcesarMovimiento()
+    private void FixedUpdate()
+    //fisicas de movimiento
     {
-        // Logica de Movimiento.
-        float inputMovimiento = Input.GetAxis("Horizontal");
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-
-        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
+        rb2d.velocity = new Vector2(Horizontal * Speed, rb2d.velocity.y);
     }
-}
-
