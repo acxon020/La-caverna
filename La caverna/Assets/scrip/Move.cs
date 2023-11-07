209d1a747443d3eb467b847f9a5f74b0d210cd0b
Gillmf19 <102002590+Gillmf19@users.noreply.github.com>
2023-11-06 23:30:41 -0800

    Updated Move.cs, Added Character Controller
    Actualización a el código de movimiento y se agregó un controlador de personaje

--- La caverna/Assets/scrip/Move.cs
+++ La caverna/Assets/scrip/Move.cs
@@ -6,44 +6,36 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     //esto aparece en script
-    public float Speed;
-    public float JumpForce;
+    public CharacterController2D controller;
+    public float Speed = 40f;
 
+    
     //esto lo mandamos a llamar adentro del juego
     private Rigidbody2D rb2d; //gravedad
-    private float Horizontal;//movimiento horizontal
-    private bool Grounded; //piso
-
+    float horizontalMove = 0f;//movimiento horizontal
+    bool Jump= false;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>(); //para que al inicio tenga gravedad
 
     }
-    //codigo de salto
+    //codigo de salto, llamado una vez por cada frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal"); //movimiento horizontal
+        horizontalMove = Input.GetAxisRaw("Horizontal")* Speed; //movimiento horizontal
 
-        Debug.DrawRay(transform.position, Vector3.down * 1.0f, Color.red);
-        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        if (Input.GetButtonDown("Jump"))
         {
-            Grounded = true;
+            Jump = true;
 
         }
-        else
-        {
-            Grounded = false;
-        }
 
-        if (Input.GetKeyDown(KeyCode.W) && Grounded) // brinco
-        {
-            rb2d.AddForce(Vector2.up * JumpForce);
-        }
     }
 
-    private void FixedUpdate()
-    //fisicas de movimiento
-    {
-        rb2d.velocity = new Vector2(Horizontal * Speed, rb2d.velocity.y);
-    }
+        void FixedUpdate()
+        //fisicas de movimiento
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, Jump);
+            Jump = false;
+        }
 }
