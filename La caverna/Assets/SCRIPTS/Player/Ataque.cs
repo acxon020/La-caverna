a7eaf06adc7a04a23023a2654a2ad16d2df8f869
Gillmf19 <102002590+Gillmf19@users.noreply.github.com>
2023-12-03 22:12:08 -0800

    enemy attack logic in process
    lógica de ataque enemigo en proceso

---
+++ La caverna/Assets/SCRIPTS/Player/Ataque.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataque : MonoBehaviour
+{
+    private int Combocounter = 0;
+    public float tiempoMaxCombo = 1f; // Tiempo máximo entre golpes para contar como parte del combo
+    private float LastHitTime;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (Time.time - LastHitTime > tiempoMaxCombo)
+        {
+            Combocounter = 0; // Restablecer el combo si ha pasado demasiado tiempo entre golpes
+        }
+
+        Combocounter++;
+
+        switch (Combocounter)
+        {
+            case 1:
+
+                animator.SetTrigger("Attack1");
+                break;
+
+            case 2:
+
+                animator.SetTrigger("Attack2");
+                break;
+
+            case 3:
+            
+                animator.SetTrigger("Attack3");
+                // Resetea el contador de combo después del tercer golpe
+                Combocounter = 0;
+                break;
+
+            default:
+              animator.SetTrigger("Attack1");
+                break;
+        }
+
+        LastHitTime = Time.time; // Actualiza el tiempo del último golpe
+    }
+}
